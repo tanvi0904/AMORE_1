cec2ad2b654158a93e654ed2b1f69f08775c282f
FlutLab.io <>
2021-02-27 10:45:52 +0000

    Autosave

--- lib/Injured.dart
+++ lib/Injured.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
-import 'main.dart';
 
 class Injured extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-       
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('TYPE OF ANIMAL'),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: NetworkImage("https://i.pinimg.com/236x/29/aa/0d/29aa0d016d0e994677556bcbaebc5e46.jpg"),
-          fit: BoxFit.cover,
-        )),
-        child: RaisedButton(
-          onPressed: () => Navigator.pop(context),
-          child: Text('Back'),
+    final title = 'Type of Animal';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://i.insider.com/5484d9d1eab8ea3017b17e29?width=600&format=jpeg&auto=webp')
+              ),
+              title: Text('Dog',style: TextStyle(fontSize:20)),
+              //trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                print('horse');
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://www.rd.com/wp-content/uploads/2019/05/American-shorthair-cat-scaled.jpg')
+              ),
+              title: Text('Cat',style: TextStyle(fontSize:20)),
+              onTap: () {
+                print('horse');
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('https://assets.rebelmouse.io/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMjU5OTUwMC9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY3MzgwMTU4OH0._5zVsVGRVsRelDSEqnrjQJlDK28WLrFpfA9kUE5gRWc/img.jpg?width=1245&quality=85&coordinates=0%2C0%2C0%2C0&height=700')
+              ),
+              title: Text('Cow',style: TextStyle(fontSize:20)),
+              onTap: () {
+                print('horse');
+              },
+            ),
+          ],
         ),
       ),
     );
