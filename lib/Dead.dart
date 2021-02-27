0b906a61e58221314d884436964de074bf690dc0
FlutLab.io <>
2021-02-27 12:16:55 +0000

    Autosave

--- lib/Dead.dart
+++ lib/Dead.dart
@@ -22,7 +22,7 @@ class Dead extends StatelessWidget {
           image: NetworkImage('https://content.thriveglobal.com/wp-content/uploads/2019/05/sadness.jpg'),
           fit: BoxFit.cover,
         )),
-        ListView(
+        /*ListView(
           children: <Widget>[
             ListTile(
               leading: CircleAvatar(
@@ -32,17 +32,14 @@ class Dead extends StatelessWidget {
               subtitle
         ),
           ],
-         /*ListTile(
+          
+         ListTile(
               leading: CircleAvatar(
                 backgroundImage: NetworkImage('https://www.rd.com/wp-content/uploads/2019/05/American-shorthair-cat-scaled.jpg')
               ),
               title: Text('pfa',style: TextStyle(fontSize:20)),
               subtitle:Text('abc')
-*/
-
-        
-
-        
+         ),
               //mainAxisAlignment: MainAxisAlignment.center,
               //crossAxisAlignment: CrossAxisAlignment.center,
               //children: <Widget>[
@@ -50,19 +47,20 @@ class Dead extends StatelessWidget {
                   height: 100.0,
                   width: 200.0,
                 */
-         /*child: ListTile(
+         child: ListTile(
           //leading: Text('BBMP crematorium'),  
           title:Text('BBMP crematorium',style: TextStyle(color: Colors.white, fontSize: 40)),
           subtitle: Text('09900707521',style: TextStyle(color: Colors.white, fontSize:36)),
           //trailing:icon(Icons.done),
-        ),*/
-          /* ListTile(
+        ),
+        /*
+           ListTile(
           
           title:Text('PFA',style: TextStyle(color: Colors.white, fontSize: 40)),
           subtitle: Text('080-2861 1986',style: TextStyle(color: Colors.white, fontSize:36)),
           
-        ),
-        */
+        ),*/
+        
                 
 /*
         SizedBox(
@@ -77,7 +75,7 @@ class Dead extends StatelessWidget {
 
               
       ),
-      ),
+      
       );
     
   }
